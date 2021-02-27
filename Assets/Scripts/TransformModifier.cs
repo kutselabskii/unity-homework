93bd839651f1820a93a9a50b06d427a14304dd5b
Egor Kutselabskii <osutanfirst@gmail.com>
2021-02-27 21:57:40 +1000

    Add cube transform changing

---
+++ Assets/Scripts/TransformModifier.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformModifier : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(30f, 15f, 10f) * Time.deltaTime);
+    }
+}
