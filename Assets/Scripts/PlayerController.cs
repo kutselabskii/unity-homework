a154442cf072da072b43aa6dccc8ae8b757af49b
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-10 23:38:50 +1000

    Add failed attempt to make camera following

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,5 +20,9 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(transform.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime);
         transform.Translate(transform.right * Input.GetAxis("Horizontal") * Speed * Time.deltaTime);
+
+        var cameraPosition = transform.position - transform.forward * 2 + new Vector3(0f, 2f, 0f);
+        Camera.transform.position = cameraPosition;
+        Camera.transform.LookAt(transform);
     }
 }
