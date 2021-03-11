d05dbed63a8df88f9938d7897c5f8ed668c02430
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-11 21:33:45 +1000

    Finish character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,27 @@ public class PlayerController : MonoBehaviour
 
     public Camera Camera;
 
+    private CharacterController controller;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        transform.Translate(transform.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime);
-        transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f));
+        //var move = transform.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        //if (move.magnitude > 0) {
+        //    Debug.Log($"F: {transform.forward} M: {move}");
+        //}
+        //transform.Translate(move);
+        //transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f));
 
-        var cameraPosition = transform.position - transform.forward * 2 + new Vector3(0f, 2f, 0f);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0);
+        var movement = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+        controller.Move(movement * Speed * Time.deltaTime);
+
+        var cameraPosition = transform.position - transform.forward * 4 + new Vector3(0f, 2f, 0f);
         Camera.transform.position = cameraPosition;
         Camera.transform.LookAt(transform);
     }
