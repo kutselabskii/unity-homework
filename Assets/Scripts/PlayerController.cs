404c866c752310e316b0ebbe0b811a50afa8bc88
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-14 23:10:12 +1000

    Add car model, update player prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,18 +18,11 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //var move = transform.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        //if (move.magnitude > 0) {
-        //    Debug.Log($"F: {transform.forward} M: {move}");
-        //}
-        //transform.Translate(move);
-        //transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f));
-
         transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0);
         var movement = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
         controller.Move(movement * Speed * Time.deltaTime);
 
-        var cameraPosition = transform.position - transform.forward * 4 + new Vector3(0f, 2f, 0f);
+        var cameraPosition = transform.position - transform.forward * 8 + new Vector3(0f, 4f, 0f);
         Camera.transform.position = cameraPosition;
         Camera.transform.LookAt(transform);
     }
