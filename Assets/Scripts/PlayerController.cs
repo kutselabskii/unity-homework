5ea28e1b94c969d3ec0b76d0b2b47f6226b2f29e
Egor Kutselabskii <osutanfirst@gmail.com>
2021-03-16 21:32:32 +1000

    Update movement script, remove character controller, add new car model, add simple road

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,17 @@ public class PlayerController : MonoBehaviour
 
     public Camera Camera;
 
-    private CharacterController controller;
-
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+
     }
 
     void Update()
     {
         transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0);
-        var movement = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
-        controller.Move(movement * Speed * Time.deltaTime);
+
+        var movement = Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal");
+        transform.Translate(movement * Speed * Time.deltaTime);
 
         var cameraPosition = transform.position - transform.forward * 8 + new Vector3(0f, 4f, 0f);
         Camera.transform.position = cameraPosition;
